b6d418cc9ff523c8c4ff733f037b7f4dec0ece1f
py4ephyopaing <py4ephyopaing@gmail.com>
2025-03-20 18:00:12 +0630

    [update] ChessSymbol Type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,5 @@
 type Move = { row: number, col: number };
 type TTurn = "white"|"black";
+type ChessSymbol = ''|'p'|'P'|'r'|'R'|'b'|'B'|'kn'|'KN'|'q'|'Q'|'k'|'K';
 
-export { Move, TTurn }
+export { Move, TTurn, ChessSymbol }
