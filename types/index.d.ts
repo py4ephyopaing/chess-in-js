afbb56faad809ba151cbf7b5b3a2460428ec8974
py4ephyopaing <py4ephyopaing@gmail.com>
2025-03-24 21:02:07 +0630

    [update] add promotion pieces as type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 type Move = { row: number, col: number };
 type Color = "white"|"black";
 type ChessSymbol = ''|'p'|'P'|'r'|'R'|'b'|'B'|'kn'|'KN'|'q'|'Q'|'k'|'K';
+type Promote = 'queen'|'knight'|'bishop'|'rook';
 
-export { Move, Color, ChessSymbol }
+export { Move, Color, ChessSymbol, Promote }
